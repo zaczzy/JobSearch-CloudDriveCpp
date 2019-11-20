929413228b8c429d8335c44a589f49e8b36729f5
Nishanth Changalath Shyamkumar <ncshy@seas.upenn.edu>
2019-11-20 15:32:15 -0500

    Updated framework for webservice

--- mailservice.c
+++ mailservice.c
@@ -7,9 +7,7 @@
 enum mail_opt {
     
     SHOW_MAIL =0,
-    READ_MAIL,
-    SEND_MAIL,
-    DELETE_MAIL,
+    READ_MAIL
 
 }mOpt;
 
@@ -27,12 +25,17 @@ int webserver_core(int mailOpt, char *user) {
 
 //Invokes backend to enquire about user
   if(user == NULL) {
+    printf("Invalid user\n");
     return -1;
   }
+
   int validate = validateUser(user);
   if(validate < 0) {
-    //TODO: Return error message
+    printf("Invalid user %s\n", user);
+    return -1;
   }
+
+  mOpt = mailOpt;
 
   switch(mOpt) {
     case SHOW_MAIL:                 //Downloads email headers from backend
@@ -48,21 +51,41 @@ int webserver_core(int mailOpt, char *user) {
 
     case READ_MAIL:
 
+      valid = validateMailId(user, id);
       //Query backend with specific ID
-      downloadMail(user, id);
+      if(valid == 0) {
+        downloadMail(user, id, email);
+        return email;
+      } else {
+        printf("Failed to read email!!\n");
+      }
       //Pack the output into a string and return. 
-      return email;
-
-
+    
+      break;
     case SEND_MAIL:
+  
+      valid = validaterUser(rcpt_user);
+      if(valid < 0) {
+        printf("No such user exists!!\n");
+        break;
+      }
 
+      sprintf(send_msg, "put <%s> <%s> <%s>", rcpt_user, mail_msg);
+      send(send_msg);
 
-
+      break;
     case DELETE_MAIL:
    
         //TODO: Request to backend to delete
+         valid = validateMailId();
+          if(valid == 0) {
+            sprintf(send_msg, "delete <username> <mailId>");
+            send();
+          } else {
+            printf("No such mail ID"); 
+          }
           //TODO: Return success or failure
-
+      break;
     default: 
       return wrong_option;
   
@@ -87,4 +110,26 @@ int validateUser(char *user) {
 
 }
 
+int validateMailId(char *user, uint16_t mailId) {
 
+  sprintf(send_msg, "validatemail <%s> <%d>", user, mailId);
+  send(send_msg);
+  recv(validatedMsg);
+
+  if(validateMsg != "SUCCESS")
+    return -1;
+  else  
+    return 0;
+
+}
+
+int downloadEmail(user, mailId, msg) {
+
+  sprintf(send_msg, "readmail <%s> <%d>", user, mailId);
+  send(send_msg);
+
+  recv(recv_msg);
+
+  printf("EMAIL\n %s", recv_msg);
+  
+}
