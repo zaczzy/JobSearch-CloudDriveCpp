580ce5346556261782733cb2cf904ff463d05515
Ritika Gupta <ritgupta@seas.upenn.edu>
2019-11-21 10:57:34 -0500

    changed the structure of email request and response

--- data_types.h
+++ data_types.h
@@ -1,6 +1,11 @@
 
+#include <vector>
+
 #define SUCCESS         0
 #define FAILURE         1
+#define MAX_LEN_EMAIL_BODY  256 // Will change this later
+#define MAX_SIZE_FILE       2048    // WIll change this later
+#define MAX_EMAILS          10      // WIll change this later
 
 #pragma pack(1)
 typedef struct
@@ -23,19 +28,31 @@ typedef struct
 #pragma pack(1)
 typedef struct
 {
-    char prefix[5];
+    char prefix[8];     // Should be "putmail"
     char username[32];
     char email_id[64];
     email_header header;
     uint64_t email_len;
-}mail_request;
+    char email_body[MAX_LEN_EMAIL_BODY];
+}put_mail_request;
 
 #pragma pack(1)
 typedef struct
 {
-    char prefix[5];
+    char prefix[8];     // Should be "getmail""
+    char username[32];
+    char email_id[64];
+    uint64_t num_emails;
+    email_header email_headers[MAX_EMAILS];
+}get_mail_request;
+
+#pragma pack(1)
+typedef struct
+{
+    char prefix[6];     // Should be "drive"
     char username[32];
     char filename[256];
     uint64_t file_len;
-}drive_request;
+    char file_content[MAX_SIZE_FILE];
+}get_file_request;
 
