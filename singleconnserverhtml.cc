8a212c8f2838374753d9af7bdbad641712b60b5e
Zachary Zhao <zhaozeyu@seas.upenn.edu>
2019-12-18 03:29:18 -0500

    untested ascii filter

--- singleconnserverhtml.cc
+++ singleconnserverhtml.cc
@@ -371,7 +371,22 @@ void SingleConnServerHTML::handleGET(bool HEAD = false) {
   sendHeaders(HTML.length());
   if (!HEAD) sendMsg(HTML);
 }
+static void ascii_filter(string& s) {
+  
+  set<string> wrong = {"+", "%21", "%22", "%23","%24","%25","%26","%27","%28","%29","%2A","%2B","%2C","%2D","%2E","%2F",
+  "%3A","%3B","%3C","%3D","%3E",
+  "%3F","%40","%5B","%5C","%5D",
+  "%5E","%5F","%60", "%7B","%7C","%7D","%7E"};
+  char right[] = {" ", '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+',',', '-', '.', '/', 
+  ':', ';', '<', '=' , '>', 
+  '?', '@', '[', '\\', ']', 
+  '^', '_', '`', '{', '|', '}', '~'};
 
+  string::size_type pos = 0;
+  while ((pos = s.find("%", pos)) != string::npos) {
+    s = s.replace(pos, sizeof(char), right[i]);
+  }
+}
 /*
  * Handle POST request
  */
@@ -501,7 +516,7 @@ void SingleConnServerHTML::handlePOST(char *body, bool is_multipart_form,
     string to = c_rcpt + strlen("to=");
     string subject = c_subj + strlen("subject=");
     string message = c_msg + strlen("message=");
-
+    ascii_filter(message);
     cout << "{" << to << "\n" << subject << "\n" << message << "}" << endl;
 
     char b[BUFF_SIZE];  // unused
