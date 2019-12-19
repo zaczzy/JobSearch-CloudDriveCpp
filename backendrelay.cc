f87172e7bca14ed3606b35534a7f810987202470
Zachary Zhao <zhaozeyu@seas.upenn.edu>
2019-12-19 17:40:27 -0500

    reconnect before testing

--- backendrelay.cc
+++ backendrelay.cc
@@ -19,83 +19,85 @@ BackendRelay::~BackendRelay() {}
  */
 void BackendRelay::setNewBackendSock(string username) {
   if (backendSock == -1) {
-    //Gimme new backend (indicate am fes)
-    unsigned short config_pair[2] = {0,0};
+    // Gimme new backend (indicate am fes)
+    unsigned short config_pair[2] = {0, 0};
     int i = write(masterSock, config_pair, sizeof(config_pair));
-    //read() error
-    if (i < -1)
-      die("write() fail to masterSock", false);
-    //client closed connection
-    if (i == 0)
-      die("masterSock closed", false);
+    if (i < -1) die("write() fail to masterSock", false);
+    // client closed connection
+    if (i == 0) die("masterSock closed", false);
   }
 
-  //Send username
+  // Send username
   char buff[BUFF_SIZE];
   sprintf(buff, "%s", (char *)username.c_str());
   int i = write(masterSock, buff, sizeof(buff));
-  if (i < -1)
-    die("write() fail to masterSock", false);
-  if (i == 0)
-    die("masterSock closed", false);
+  if (i < -1) die("write() fail to masterSock", false);
+  if (i == 0) die("masterSock closed", false);
 
-  if (VERBOSE)
-    cout << "master written bytes: " << i << endl;
+  if (VERBOSE) cout << "master written bytes: " << i << endl;
 
-	//Backend gimme'd
-	struct sockaddr_in newAddr;
-	i = read(masterSock, &newAddr, sizeof(newAddr));
-	if (i < -1)
-		die("read() fail to masterSock", false);
-	if (i == 0)
-		die("masterSock closed", false);
+  // Backend gimme'd
+  struct sockaddr_in newAddr;
+  i = read(masterSock, &newAddr, sizeof(newAddr));
+  if (i < -1) die("read() fail to masterSock", false);
+  if (i == 0) die("masterSock closed", false);
 
-  if (VERBOSE)
-	  cout << "master read" << endl;
+  if (VERBOSE) cout << "master read" << endl;
 
-  cout << inet_ntoa(newAddr.sin_addr) << "\n"<< ntohs(newAddr.sin_port) << endl;
+  cout << inet_ntoa(newAddr.sin_addr) << "\n"
+       << ntohs(newAddr.sin_port) << endl;
 
-  if (backendSock > 0)
-    close(backendSock);
+  if (backendSock > 0) close(backendSock);
 
-	//Create new backend sock
-	backendSock = socket(AF_INET, SOCK_STREAM, 0);
-	if (backendSock < 0)
-		die("socket() failed", -1);
+  // Create new backend sock
+  backendSock = socket(AF_INET, SOCK_STREAM, 0);
+  if (backendSock < 0) die("socket() failed", -1);
 
-	cout << "backend made " << backendSock << endl;
+  cout << "backend made " << backendSock << endl;
 
-	//Reusable
-	int enable = 1;
-	if (setsockopt(backendSock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
-		die("setsockopt(SO_REUSEADDR) failed", false);
-	if (setsockopt(backendSock, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int)) < 0)
-		die("setsockopt(SO_REUSEPORT) failed", false);
+  // Reusable
+  int enable = 1;
+  if (setsockopt(backendSock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) <
+      0)
+    die("setsockopt(SO_REUSEADDR) failed", false);
+  if (setsockopt(backendSock, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int)) <
+      0)
+    die("setsockopt(SO_REUSEPORT) failed", false);
 
-	//Connect
-	if (connect(backendSock, (struct sockaddr*)&newAddr, sizeof(newAddr)) < 0)
-		die("connect failed", false);
-//	return backendSock;
+  // Connect
+  if (connect(backendSock, (struct sockaddr *)&newAddr, sizeof(newAddr)) < 0)
+    die("connect failed", false);
+  //	return backendSock;
 
-	//Clear welcome message from backend socket
-	memset(buff, 0, sizeof(buff));
-	read(backendSock, buff, sizeof(buff));
-	if (VERBOSE)
-		fprintf(stderr, "%s\n", buff);
+  // Clear welcome message from backend socket
+  memset(buff, 0, sizeof(buff));
+  read(backendSock, buff, sizeof(buff));
+  if (VERBOSE) fprintf(stderr, "%s\n", buff);
 
-	if (VERBOSE)
-		fprintf(stderr, "S: connected to [%d]! (B)\n", backendSock);
+  if (VERBOSE) fprintf(stderr, "S: connected to [%d]! (B)\n", backendSock);
 }
 
 /*
  * Send command to backend (ritika)
  */
-string BackendRelay::sendCommand(string command) {
-  char* c_command = (char*)command.c_str();
+string BackendRelay::sendCommand(string &command, const string &username) {
+  char *c_command = (char *)command.c_str();
   char buff[BUFF_SIZE];
-  write(backendSock, c_command, strlen(c_command));
   if (VERBOSE) fprintf(stderr, "[%d][BACK] S: %s\n", backendSock, c_command);
-  read(backendSock, buff, sizeof(buff));
+  write(backendSock, c_command, strlen(c_command));
+  int size_read = read(backendSock, buff, sizeof(buff));
+  // reconnect
+  while (size_read == 0) {
+    pthread_mutex_lock(&mutex_sock);
+    setNewBackendSock(username);
+    pthread_mutex_unlock(&mutex_sock);
+    // do the same thing again
+    memset(buff, 0, BUFF_SIZE);
+    if (VERBOSE)
+      fprintf(stderr, "[%d][NEWBACK] S: %s\n", backendSock, c_command);
+    write(backendSock, c_command, strlen(c_command));
+    size_read = read(backendSock, buff, sizeof(buff));
+  }
   if (VERBOSE) fprintf(stderr, "[%d][BACK] C: %s\n", backendSock, buff);
   string result = buff;
   return result;
@@ -104,19 +106,35 @@ string BackendRelay::sendCommand(string command) {
 int BackendRelay::getSock() { return backendSock; }
 
 // RESPONSE be like "Ffile1~Ddir1~Ffile2~" or "~" when empty
-string BackendRelay::sendFolderRequest(const get_folder_content_request* req,
-                                       size_t max_resp_len) {
+string BackendRelay::sendFolderRequest(const get_folder_content_request *req,
+                                       size_t max_resp_len,
+                                       const string &username) {
+  if (VERBOSE)
+    fprintf(stderr, "[%d][BACK] S: Folder request sent\n", backendSock);
   write(backendSock, req, sizeof(*req));
-  char* buff = new char[max_resp_len];
+  char *buff = new char[max_resp_len];
   int buff_size = read(backendSock, buff, max_resp_len);
+  // reconnect
+  while (buff_size == 0) {
+    pthread_mutex_lock(&mutex_sock);
+    setNewBackendSock(username);
+    pthread_mutex_unlock(&mutex_sock);
+    // do the same thing again
+    memset(buff, 0, max_resp_len);
+    if (VERBOSE)
+      fprintf(stderr, "[%d][NEWBACK] S: Folder request sent\n", backendSock);
+    write(backendSock, req, sizeof(*req));
+    buff_size = read(backendSock, buff, max_resp_len);
+  }
   string result;
   if ((size_t)buff_size < max_resp_len) {
     buff[buff_size] = '\0';
+    if (VERBOSE) fprintf(stderr, "[%d][BACK] C: %s\n", backendSock, buff);
     result = buff;
     if (buff_size == 1 && buff[0] == '~') {
       return "";
     } else if (buff[buff_size - 1] != '~') {
-      cerr<< "getFolderContentRequest response malformated" << endl;
+      cerr << "getFolderContentRequest response malformated" << endl;
       cerr << "|" << result << "|" << endl;
       result = "";
     }
@@ -128,12 +146,12 @@ string BackendRelay::sendFolderRequest(const get_folder_content_request* req,
   delete[] buff;
   return result;
 }
-void BackendRelay::sendFileRequest(const get_file_request* req) {
+void BackendRelay::sendFileRequest(const get_file_request *req) {
   write(backendSock, req, sizeof(*req));
 }
-bool BackendRelay::sendChunk(const string& username,
-                             const string& directory_path,
-                             const string& filename, const string& data,
+bool BackendRelay::sendChunk(const string &username,
+                             const string &directory_path,
+                             const string &filename, const string &data,
                              const size_t chunk_len) {
   put_file_request req;
   memset(&req, 0, sizeof(req));
@@ -149,6 +167,8 @@ bool BackendRelay::sendChunk(const string& username,
   printf("%lu\n", sizeof(req));
   write(backendSock, &req, sizeof(req));
   char buff[50];
+  // cannot afford to worry about switching backend during multi-chunk upload
+  // assuming neven kills backend during upload process
   int resp_size = read(backendSock, &buff, 50);
   buff[resp_size] = '\0';
   if (strncmp(buff, "+OK", 3) == 0) {
@@ -158,9 +178,12 @@ bool BackendRelay::sendChunk(const string& username,
   return false;
 }
 // TODO: ask ritika about confirm
-bool BackendRelay::createFolderRequest(const create_folder_request* req) {
+bool BackendRelay::createFolderRequest(const create_folder_request *req) {
   write(backendSock, req, sizeof(*req));
-  char* confirm = new char[1024];
+  char *confirm = new char[1024];
+  // assuming the creation happens in a short time
+  // killing the backend cannot happen before it can send ACK
+  // not within scope of project
   int rlen = read(backendSock, confirm, 1024);
   confirm[rlen] = '\0';
   printf("mkfolder confirm: %s\n", confirm);
@@ -170,9 +193,12 @@ bool BackendRelay::createFolderRequest(const create_folder_request* req) {
 }
 // TODO: ask ritika about confirm
 bool BackendRelay::removeFolderRequest(
-    const delete_folder_content_request* req) {
+    const delete_folder_content_request *req) {
   write(backendSock, req, sizeof(*req));
-  char* confirm = new char[1024];
+  char *confirm = new char[1024];
+  // assuming the deletion happens in a short time
+  // killing the backend cannot happen before it can send ACK
+  // not within scope of project
   int rlen = read(backendSock, confirm, 1024);
   confirm[rlen] = '\0';
   printf("confirm: %s\n", confirm);
@@ -180,6 +206,8 @@ bool BackendRelay::removeFolderRequest(
   delete[] confirm;
   return true;
 }
-void BackendRelay::recvChunk(get_file_response* resp) {
+void BackendRelay::recvChunk(get_file_response *resp) {
+  // cannot afford to worry about switching backend during multi-chunk download
+  // assuming neven kills backend during download process
   read(backendSock, resp, sizeof(*resp));
 };
